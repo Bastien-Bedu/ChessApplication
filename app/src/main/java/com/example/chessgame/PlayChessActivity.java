017afdc4ef67813e171832307ca04523202dd0b0
Bastien-Bedu <bastien.bedu@epitech.eu>
2021-02-15 23:03:17 +0200

    Pieces now able to move (without any game rules)

--- app/src/main/java/com/example/chessgame/PlayChessActivity.java
+++ app/src/main/java/com/example/chessgame/PlayChessActivity.java
@@ -2,13 +2,22 @@ package com.example.chessgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.graphics.Color;
 import android.graphics.ColorMatrixColorFilter;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.DragEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.example.chessgame.chess.ChessBoard;
 import com.example.chessgame.chess.Implementation;
@@ -17,8 +26,9 @@ import com.example.chessgame.chess.Tile;
 
 import java.util.ArrayList;
 
-public class PlayChessActivity extends AppCompatActivity {
+public class PlayChessActivity extends AppCompatActivity implements View.OnDragListener, View.OnTouchListener {
 
+    private float xCoordinate, yCoordinate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,17 +61,20 @@ public class PlayChessActivity extends AppCompatActivity {
                         black ? R.color.blackTile : R.color.whiteTile
                 ));
                 frameLayout.setLayoutParams(layoutParams);
+                frameLayout.setOnDragListener(this);
+                frameLayout.setClipChildren(false);
 
                 if (!tile.isEmpty()) {
                     ImageView imageView = new ImageView(this);
+                    imageView.setOnTouchListener(this);
                     imageView.setImageResource(Implementation.typeToAsset.get(tile.getPieceType()));
                     if (tile.getPlayerColor() == PlayerColor.BLACK) {
                         //imageView.setColorFilter(getResources().getColor(R.color.black));
                         float[] negative = {
-                                -1.0f,     0,     0,    0, 255, // red
-                                0, -1.0f,     0,    0, 255, // green
-                                0,     0, -1.0f,    0, 255, // blue
-                                0,     0,     0, 1.0f,   0  // alpha
+                                -1.0f, 0, 0, 0, 255, // red
+                                0, -1.0f, 0, 0, 255, // green
+                                0, 0, -1.0f, 0, 255, // blue
+                                0, 0, 0, 1.0f, 0  // alpha
                         };
 
                         imageView.setColorFilter(new ColorMatrixColorFilter(negative));
@@ -71,16 +84,91 @@ public class PlayChessActivity extends AppCompatActivity {
                     imageView.setLayoutParams(lpImage);
                     frameLayout.addView(imageView);
                 }
-
-
                 chessBoardLayout.addView(frameLayout);
                 black = !black;
             }
             black = !black;
         }
 
-
-
-
     }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            ClipData data = ClipData.newPlainText("", "");
+            View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(
+                    view);
+            view.setVisibility(View.INVISIBLE);
+            view.startDragAndDrop(data, shadowBuilder, view, 0);
+            return true;
+        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+            view.performClick();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    @Override
+    public boolean onDrag(View v, DragEvent event) {
+
+        // Store the action type for the incoming event
+        int action = event.getAction();
+
+        // Handles each of the expected events
+        switch (action) {
+            case DragEvent.ACTION_DRAG_STARTED:
+                return true;
+
+            case DragEvent.ACTION_DRAG_ENTERED:
+
+                // Apply a gray tint to the View
+                v.getBackground().setColorFilter(Color.GRAY, PorterDuff.Mode.SRC_IN);
+
+                // Invalidate the view to force a redraw in the new tint
+                v.invalidate();
+
+                return true;
+
+            case DragEvent.ACTION_DRAG_LOCATION:
+
+                // Ignore the event
+                return true;
+
+            case DragEvent.ACTION_DRAG_EXITED:
+
+                // Re-sets the color tint to yellow
+                v.getBackground().clearColorFilter();
+
+                // Invalidate the view to force a redraw in the new tint
+                v.invalidate();
+
+                return true;
+
+            case DragEvent.ACTION_DROP:
+                v.getBackground().clearColorFilter();
+
+                View view = (View) event.getLocalState();
+                ViewGroup owner = (ViewGroup) view.getParent();
+                owner.removeView(view);
+                FrameLayout container = (FrameLayout) v;
+                container.addView(view);
+                view.setVisibility(View.VISIBLE);
+
+                v.invalidate();
+
+                return true;
+
+            case DragEvent.ACTION_DRAG_ENDED:
+
+                return true;
+
+            default:
+                break;
+        }
+
+        return false;
+    }
+
 }
